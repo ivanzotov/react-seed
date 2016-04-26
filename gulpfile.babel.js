f7b12ddba813d0c915b46ba7eead71ee3f306df0
Ivan Zotov <ivanzotov@gmail.com>
2016-04-26 17:40:24 +0300

    Improve glob, watch and babel

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import gulp from 'gulp';
 import fs from 'fs';
 import del from 'del';
@@ -12,6 +13,8 @@ import sourcemaps from 'gulp-sourcemaps';
 import rev from 'gulp-rev';
 import revReplace from 'gulp-rev-replace';
 import runSequence from 'run-sequence';
+import glob from 'glob';
+import es from 'event-stream';
 
 let debug, watch = false;
 gulp.task('mode:debug', () => debug = true);
@@ -26,22 +29,22 @@ let bucket_region = 'us-east-1';
 
 let log = (msg) => gutil.log(gutil.colors.green(msg));
 
-let fonts_src = './src/fonts/**/*',
+let src = './src/',
+    fonts_src = './src/fonts/**/*',
     fonts_dest = './dist/fonts/',
     images_src = './src/images/**/*.{jpg,jpeg,png,gif,svg}',
     images_dest = './dist/images/',
-    styles_src = './src/styles/**/*.{css,scss}',
-    scripts_src = './src/scripts/**/*.js',
-    scripts_filename = 'base.js',
-    scripts_dest = './dist/scripts/',
-    styles_filename = 'base.css',
+    styles_src = './src/styles/*.{css,scss}',
+    styles_watch_src = 'styles/**/*.{css,scss}',
     styles_dest = './dist/styles/',
-    html_filename = 'index.html',
+    scripts_src = './src/scripts/*.js',
+    scripts_watch_src = 'scripts/**/*.js',
+    scripts_dest = './dist/scripts/',
     html_dest = './dist/';
 
 gulp.task('clean:html', () => {
   log('HTML clean from dest folder');
-  return del.sync([html_dest+html_filename], {force: true});
+  return del.sync([html_dest+'*.html'], {force: true});
 });
 
 gulp.task('clean:scripts', () => {
@@ -70,19 +73,28 @@ gulp.task('build', (cb) => {
   runSequence('images:build', 'scripts:build', 'styles:build', cb);
 });
 
-gulp.task('compile', ['clean:html', 'build'], () => {
-  let App = require('./index'),
-      React = require('react'),
+gulp.task('compile', ['clean:html', 'build'], (cb) => {
+  let React = require('react'),
       ReactDOM = require('react-dom/server'),
       htmlmin = require('gulp-htmlmin');
 
-  let stream = source(html_filename);
-  log(`Destination HTML file written ${html_dest}${html_filename}`);
+  glob('./src/pages/*.js', (err, files) => {
+    if(err) cb(err);
 
-  stream.end(`<!DOCTYPE html>${ReactDOM.renderToStaticMarkup(React.createElement(App))}`);
-  return stream.pipe(streamify(gulpif(!debug, htmlmin({collapseWhitespace: true}))))
-               .pipe(streamify(gulpif(!debug, revReplace({manifest: manifest()}))))
-               .pipe(gulp.dest(html_dest));
+    let tasks = files.map((page) => {
+      let Page = require(page).default;
+      let filename = page.replace(/^.*(\\|\/|\:)/, '').replace('.js', '.html');
+      let stream = source(filename);
+      log(`Destination HTML file written ${html_dest}${filename}`);
+
+      stream.end(`<!DOCTYPE html>${ReactDOM.renderToStaticMarkup(React.createElement(Page))}`);
+      return stream.pipe(streamify(gulpif(!debug, htmlmin({collapseWhitespace: true}))))
+                   .pipe(streamify(gulpif(!debug, revReplace({manifest: manifest()}))))
+                   .pipe(gulp.dest(html_dest));
+    });
+
+    es.merge(tasks).on('end', cb);
+  });
 });
 
 gulp.task('fonts:build', ['clean:fonts'], () => {
@@ -102,75 +114,99 @@ gulp.task('images:build', ['clean:images'], () => {
              .pipe(gulpif(!debug, gulp.dest(html_dest)));
 });
 
-gulp.task('scripts:build', ['clean:scripts'], () => {
+gulp.task('scripts:build', ['clean:scripts'], (cb) => {
   let watchify = require('watchify'),
       uglify = require('gulp-uglify'),
+      babelify = require('babelify'),
+      reactify = require('reactify'),
       browserify = require('browserify');
 
-  let browserifyOpts = {debug: debug, entries: 'routes.js', extensions: ['.js']};
-  let opts = Object.assign({}, watchify.args, browserifyOpts);
-  let b = watch ? watchify(browserify(opts)) : browserify(opts);
+  glob(scripts_src, (err, files) => {
+    if(err) cb(err);
 
-  let bundle = () => {
-    log(`Javascript files prepared and written to ${scripts_dest}${scripts_filename}`);
+    let tasks = files.map((entry) => {
+      let browserifyOpts = {debug: debug, entries: [entry], extensions: ['.js']};
+      let opts = Object.assign({}, watchify.args, browserifyOpts);
+      let b = watch ? watchify(browserify(opts)) : browserify(opts);
+      b.transform(babelify);
+      let filename = entry.replace(/^.*(\\|\/|\:)/, '');
 
-    return b.bundle()
-    .pipe(source(scripts_filename))
-    .pipe(buffer())
-    .pipe(gulpif(debug, sourcemaps.init({loadMaps: true})))
-    .pipe(gulpif(!debug, revReplace({manifest: manifest()})))
-    .pipe(uglify())
-    .pipe(gulpif(!debug, rev()))
-    .pipe(gulpif(debug, sourcemaps.write()))
-    .pipe(gulp.dest(scripts_dest))
-    .pipe(gulpif(!debug, rev.manifest({base: './dist', merge: true})))
-    .pipe(gulpif(!debug, gulp.dest(html_dest)));
-  }
+      let bundle = () => {
+        log(`Javascript files prepared and written to ${scripts_dest}${filename}`);
 
-  if (watch) {
-    b.on('update', bundle);
-    b.on('error', gutil.log.bind(gutil, 'Browserify Error'));
-    b.on('log', gutil.log);
-  }
+        return b.bundle()
+        .pipe(source(filename))
+        .pipe(buffer())
+        .pipe(gulpif(debug, sourcemaps.init({loadMaps: true})))
+        .pipe(gulpif(!debug, revReplace({manifest: manifest()})))
+        .pipe(uglify())
+        .pipe(gulpif(!debug, rev()))
+        .pipe(gulpif(debug, sourcemaps.write()))
+        .pipe(gulp.dest(scripts_dest))
+        .pipe(gulpif(!debug, rev.manifest({base: './dist', merge: true})))
+        .pipe(gulpif(!debug, gulp.dest(html_dest)));
+      }
 
-  return bundle();
+      if (watch) {
+        b.on('update', bundle);
+        b.on('error', gutil.log.bind(gutil, 'Browserify Error'));
+        b.on('log', gutil.log);
+      }
+
+      return bundle();
+    });
+
+    es.merge(tasks).on('end', cb);
+  });
 });
 
 gulp.task('scripts:watch', ['mode:debug', 'mode:watch'], () => {
-  return gulp.watch(scripts_src, ['scripts:build']);
+  return gulp.watch(scripts_watch_src, {cwd: src}, ['scripts:build']);
 });
 
-gulp.task('styles:build', ['clean:styles'], () => {
+gulp.task('styles:build', ['clean:styles'], (cb) => {
   let postcss = require('gulp-postcss'),
       syntax = require('postcss-scss'),
       autoprefixer = require('autoprefixer'),
+      sassGlob = require('gulp-sass-glob'),
       sass = require('gulp-sass'),
-      concat = require('gulp-concat'),
+      ext_replace = require('gulp-ext-replace'),
       minify = require('gulp-minify-css');
 
   let processors = [
     autoprefixer({ browsers: ['last 2 versions'] })
   ];
 
-  log(`CSS files processed and concatenated to ${styles_dest}${styles_filename}`);
+  log(`CSS files processed and concatenated to ${styles_dest}`);
 
-  return gulp.src(styles_src)
-    .pipe(gulpif(debug, sourcemaps.init()))
-    .pipe(postcss(processors, {syntax: syntax}))
-    .pipe(concat(styles_filename))
-    .pipe(sass({indentedSyntax: false, errLogToConsole: true}))
-    .pipe(gulpif(!debug, revReplace({manifest: manifest()})))
-    .pipe(minify())
-    .pipe(rename(styles_filename))
-    .pipe(gulpif(debug, sourcemaps.write()))
-    .pipe(gulpif(!debug, rev()))
-    .pipe(gulp.dest(styles_dest))
-    .pipe(gulpif(!debug, rev.manifest({base: './dist', merge: true})))
-    .pipe(gulpif(!debug, gulp.dest(html_dest)));
+  glob(styles_src, (err, files) => {
+    if (err) cb(err);
+
+    let tasks = files.map((entry) => {
+      let filename = entry.replace(/^.*(\\|\/|\:)/, '');
+
+      return gulp.src(entry)
+        .pipe(gulpif(debug, sourcemaps.init()))
+        .pipe(postcss(processors, {syntax: syntax}))
+        .pipe(sassGlob())
+        .pipe(sass({indentedSyntax: false, errLogToConsole: true}))
+        .pipe(gulpif(!debug, revReplace({manifest: manifest()})))
+        .pipe(minify())
+        .pipe(rename(filename))
+        .pipe(ext_replace('.css'))
+        .pipe(gulpif(debug, sourcemaps.write()))
+        .pipe(gulpif(!debug, rev()))
+        .pipe(gulp.dest(styles_dest))
+        .pipe(gulpif(!debug, rev.manifest({base: './dist', merge: true})))
+        .pipe(gulpif(!debug, gulp.dest(html_dest)));
+    });
+
+    es.merge(tasks).on('end', cb);
+  });
 });
 
 gulp.task('styles:watch', ['mode:debug', 'mode:watch'], () => {
-  return gulp.watch(styles_src, ['styles:build']);
+  return gulp.watch(styles_watch_src, {cwd: src}, ['styles:build']);
 });
 
 gulp.task('publish', ['compile'], () => {
